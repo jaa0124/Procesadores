0d09df5cd952ae59c1a21afce187db970a98e562
Johnson <theacaro05@gmail.com>
2019-10-05 12:58:14 +0200

    P01 started

--- Flex/Practica1/lex.yy.c
+++ Flex/Practica1/lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
+#define YY_NUM_RULES 6
+#define YY_END_OF_BUFFER 7
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,9 +360,11 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[11] =
+static const flex_int16_t yy_accept[30] =
     {   0,
-        0,    0,    5,    4,    3,    4,    1,    2,    1,    0
+        0,    0,    0,    0,    7,    5,    6,    4,    5,    3,
+        4,    4,    0,    0,    4,    0,    2,    0,    0,    0,
+        0,    0,    0,    0,    0,    0,    0,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -371,15 +373,15 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
-        1,    1,    1,    1,    4,    4,    4,    4,    4,    4,
-        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
-        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        1,    1,    1,    1,    3,    4,    1,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    6,    1,    7,    1,
 
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        8,    9,    1,    1,    1,    1,    1,   10,    1,    1,
+       11,   12,    1,    1,   13,   14,    1,    1,    1,   15,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,33 +399,42 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[5] =
+static const YY_CHAR yy_meta[16] =
     {   0,
-        1,    1,    2,    3
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[13] =
+static const flex_int16_t yy_base[31] =
     {   0,
-        0,    0,    7,    8,    8,    0,    0,    8,    0,    8,
-        4,    2
+        0,    7,    1,    2,   33,   34,   34,   27,   16,   34,
+        6,   25,   17,   23,    9,   19,   21,   18,   10,   10,
+       19,   12,   10,    8,   12,    3,    3,   34,   34,    0
     } ;
 
-static const flex_int16_t yy_def[13] =
+static const flex_int16_t yy_def[31] =
     {   0,
-       10,    1,   10,   10,   10,   11,   12,   10,   12,    0,
-       10,   10
+       30,   30,    2,    2,   29,   29,   29,   29,   29,   29,
+       29,   29,   29,   29,   29,   29,   29,   29,   29,   29,
+       29,   29,   29,   29,   29,   29,   29,   29,    0,   29
     } ;
 
-static const flex_int16_t yy_nxt[13] =
+static const flex_int16_t yy_nxt[50] =
     {   0,
-        4,    5,    6,    7,    9,    8,   10,    3,   10,   10,
-       10,   10
+        6,    7,   10,   10,    8,   11,   11,    9,    7,   14,
+       15,    8,   14,   15,    9,   28,   27,   26,   25,   24,
+       23,   22,   21,   20,   19,   17,   18,   17,   16,   12,
+       13,   12,   29,    5,   29,   29,   29,   29,   29,   29,
+       29,   29,   29,   29,   29,   29,   29,   29,   29
     } ;
 
-static const flex_int16_t yy_chk[13] =
+static const flex_int16_t yy_chk[50] =
     {   0,
-        1,    1,    1,    1,   12,   11,    3,   10,   10,   10,
-       10,   10
+       30,    1,    3,    4,    1,    3,    4,    1,    2,   11,
+       11,    2,   15,   15,    2,   27,   26,   25,   24,   23,
+       22,   21,   20,   19,   18,   17,   16,   14,   13,   12,
+        9,    8,    5,   29,   29,   29,   29,   29,   29,   29,
+       29,   29,   29,   29,   29,   29,   29,   29,   29
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -440,16 +451,19 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "p1.l"
-#line 2 "p1.l"
+#line 1 "ejemplo_estados.l"
+/*Escáner que proporciona 2 interpretaciones del siguiete string: "123.456".*/
+/*Por defecto lo tratará como 3 diferentes tokens: el entero 123, el punto . y el entero 456*/
+/*Pero si el string es precediido por el string "expects-floats", lo tratará como un único lexema*/
+/*el float 123.456*/
+#line 7 "ejemplo_estados.l"
+#include <math.h>
+#line 462 "lex.yy.c"
 
-int pMayusc = 0;
-int n2Cifras = 0;
-int linea = 1;
-#line 450 "lex.yy.c"
-#line 451 "lex.yy.c"
+#line 464 "lex.yy.c"
 
 #define INITIAL 0
+#define expect 1
 
 #ifndef YY_NO_UNISTD_H
 /* Special case for "unistd.h", since it is non-ANSI. We include it way
@@ -664,9 +678,9 @@ YY_DECL
 		}
 
 	{
-#line 9 "p1.l"
+#line 10 "ejemplo_estados.l"
 
-#line 670 "lex.yy.c"
+#line 684 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -693,13 +707,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 11 )
+				if ( yy_current_state >= 30 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 8 );
+		while ( yy_base[yy_current_state] != 34 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -725,27 +739,44 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 10 "p1.l"
-{ pMayusc++; }
+#line 11 "ejemplo_estados.l"
+BEGIN(expect);
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 11 "p1.l"
-{ n2Cifras; }
+#line 12 "ejemplo_estados.l"
+{
+    printf("found a float, = %f\n", atof(yytext));
+    }
 	YY_BREAK
 case 3:
 /* rule 3 can match eol */
 YY_RULE_SETUP
-#line 12 "p1.l"
-{ printf("\tSalto de línea %d\n",linea); linea++; }
+#line 15 "ejemplo_estados.l"
+{
+    BEGIN(INITIAL);
+    }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 13 "p1.l"
+#line 18 "ejemplo_estados.l"
+{
+    printf("found an integer, = %d\n", atoi(yytext));
+}
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 21 "ejemplo_estados.l"
+{ printf("found a dot\n"); }
+	YY_BREAK
+case 6:
+YY_RULE_SETUP
+#line 22 "ejemplo_estados.l"
 ECHO;
 	YY_BREAK
-#line 748 "lex.yy.c"
+#line 778 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
+case YY_STATE_EOF(expect):
 	yyterminate();
 
 	case YY_END_OF_BUFFER:
@@ -1041,7 +1072,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 11 )
+			if ( yy_current_state >= 30 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1069,11 +1100,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 11 )
+		if ( yy_current_state >= 30 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 10);
+	yy_is_jam = (yy_current_state == 29);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1749,9 +1780,5 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 13 "p1.l"
+#line 22 "ejemplo_estados.l"
 
-int main(){
-    yylex();
-    printf("# de palabras mayúsculas: %d\n# de números de 2 cifras: %d\n", pMayusc, n2Cifras);
-}
